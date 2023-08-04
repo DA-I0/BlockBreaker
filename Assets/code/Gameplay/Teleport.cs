09e05bd426e65e101316a6d42271d475fdf67a0e
DA-I0 <trashpandaofdeath@protonmail.com>
2023-08-04 20:08:14 +0200

    Teleport refactor, trigger adjustments

--- Assets/code/Gameplay/Teleport.cs
+++ Assets/code/Gameplay/Teleport.cs
@@ -2,28 +2,28 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-	[SerializeField] private bool _active;
+	[SerializeField] private bool _isActive;
 	[SerializeField] private GameObject _door;
 	[SerializeField] private Transform _linkedDoor;
 	[SerializeField] private float _ballDisplacement = 0.6f;
 	[SerializeField] private float _reactivationDelay = 3f;
 	private BoxCollider2D _trigger;
 
-	public void UpdateDoorState(bool isActive, bool updateLinked = true)
+	public void UpdateDoorState(bool setActive, bool updateLinked = true)
 	{
-		_active = isActive;
-		_door.SetActive(!isActive);
-		_trigger.enabled = isActive;
+		_isActive = setActive;
+		_door.SetActive(!_isActive);
+		_trigger.enabled = _isActive;
 
 		if (updateLinked)
 		{
-			_linkedDoor.GetComponent<Teleport>().UpdateDoorState(isActive, false);
+			_linkedDoor.GetComponent<Teleport>().UpdateDoorState(_isActive, false);
 		}
 	}
 
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
-		if (_active && collider.gameObject.tag == "ball")
+		if (_isActive && collider.gameObject.tag == "ball")
 		{
 			Transform ball = collider.transform;
 
@@ -39,7 +39,7 @@ public class Teleport : MonoBehaviour
 
 	private void OnTriggerExit2D(Collider2D collider)
 	{
-		if (!_active && collider.gameObject.tag == "ball")
+		if (!_isActive && collider.gameObject.tag == "ball")
 		{
 			Invoke("DelayActivation", _reactivationDelay);
 		}
