302ba9a70246c95a7a77066a2d5953a2bbc5a8ef
DA-I0 <trashpandaofdeath@protonmail.com>
2023-11-01 15:27:35 +0100

    Made death marker into last life sign, single copy

--- code/system/LevelManager.cs
+++ code/system/LevelManager.cs
@@ -66,8 +66,6 @@ public partial class LevelManager : Node
 		{
 			Node2D mapBackground = (Node2D)newScene.GetChild(0);
 			mapBackground.Modulate = new Color(mapBackground.Modulate.R, mapBackground.Modulate.G, mapBackground.Modulate.B, refs.settings.BackgroundBrightness);
-			Node2D deathPitAnimation = (Node2D)newScene.GetChild(2);
-			deathPitAnimation.Modulate = new Color(deathPitAnimation.Modulate.R, deathPitAnimation.Modulate.G, deathPitAnimation.Modulate.B, refs.settings.HelperTransparency);
 		}
 
 		currentScene.CallDeferred("add_child", newScene);
