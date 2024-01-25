049c0c21838a3cf4d0b56a6819be962014303bf2
DA-I0 <trashpandaofdeath@protonmail.com>
2024-01-25 17:22:00 +0100

    Added skill icon, changed to use auto font variant application

--- code/ui/UISkillPanel.cs
+++ code/ui/UISkillPanel.cs
@@ -4,6 +4,7 @@ namespace BoGK.UI
 {
 	public partial class UISkillPanel : UIPaginatorPanel
 	{
+		[Export] private TextureRect _skillIcon;
 		[Export] private Label _name;
 		[Export] private Label _description;
 
@@ -20,7 +21,11 @@ namespace BoGK.UI
 		{
 			Skill currentSkill = refs.gameData.Skills[_currentSkill];
 			_name.Text = $"SKILL_{currentSkill.ToString().ToUpper()}_NAME";
-			_description.Text = $"{Tr("SKILL_ACTIVATION_COST")}: {currentSkill.ActivationCost}\n{Tr($"SKILL_{currentSkill.ToString().ToUpper()}_DESC")}";
+			_description.Text = $"{Tr("SKILL_ACTIVATION_COST")}: {currentSkill.ActivationCost}\n\n{Tr($"SKILL_{currentSkill.ToString().ToUpper()}_DESC")}";
+
+			string activeSkillIconPath = $"{ProjectSettings.GetSetting("global/SkillIconsFilePath")}/skill_{currentSkill.GetType()}.png";
+			_skillIcon.Texture = ResourceLoader.Load<Texture2D>(activeSkillIconPath);
+
 			UpdatePaginatorStatus(_currentSkill);
 		}
 
