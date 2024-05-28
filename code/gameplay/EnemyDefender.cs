00ff817b4bdfd6a9e6275d56251e4fabdbb878d9
DA-I0 <trashpandaofdeath@protonmail.com>
2024-05-28 17:37:08 +0200

    Added knockback to defender enemy type on shield hit

--- code/gameplay/EnemyDefender.cs
+++ code/gameplay/EnemyDefender.cs
@@ -12,6 +12,31 @@ namespace BoGK.Gameplay
 			}
 
 			PauseMovement();
+			AnimateShieldHit();
+			Vector2 newVelocity = _moveDirection * -1;
+			KinematicCollision2D collision = MoveAndCollide(newVelocity, false, 0.01f);
+		}
+
+		private void AnimateShieldHit()
+		{
+			switch (SpriteDirection())
+			{
+				case Vector2(0f, -1f):
+					_animator.Play("brace_up");
+					break;
+
+				case Vector2(-1f, 0f):
+					_animator.Play("brace_left");
+					break;
+
+				case Vector2(1f, 0f):
+					_animator.Play("brace_right");
+					break;
+
+				default:
+					_animator.Play("brace_down");
+					break;
+			}
 		}
 	}
 }
